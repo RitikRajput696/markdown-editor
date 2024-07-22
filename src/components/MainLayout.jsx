c6c2a2c72c07a793215e8a19690317e06e1b37b9
RitikRajput696 <rixusinput696@gmail.com>
2024-07-22 22:34:19 +0530

    added download button

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainLayout({ children }) {
   return (
-    <main className="grid grid-cols-2 h-full bg-gray-900 text-gray-400">
+    <main className="grid sm:grid-cols-2 grid-cols-1 h-full bg-gray-900 text-gray-400">
       {children}
     </main>
   );
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -6,10 +6,25 @@ import remarkGfm from "remark-gfm";
 function Preview() {
   const [markdown] = useContext(markdownContext);
 
+  const downloadFile = () => {
+    const link = document.createElement("a");
+    const file = new Blob([markdown], { type: "text/plain" });
+    link.href = URL.createObjectURL(file);
+    link.download = "Untitled.md";
+    link.click();
+    URL.revokeObjectURL(link.href);
+  };
+
   return (
     <div className="flex flex-col h-full overflow-auto">
-      <div className="flex items-center content-center min-h-10 bg-gray-700  text-gray-400 font-normal border-r-2 border-r-gray-400 sticky top-0">
-        <span className="ml-4"> PREVIEW</span>
+      <div className="flex items-center  justify-between min-h-10 bg-gray-700  text-gray-400 font-normal border-r-2 border-r-gray-400 sticky top-0 pl-4 pr-4">
+        <span className="block"> PREVIEW</span>
+        <button
+          className="bg-orange-600 text-gray-200 p-2"
+          onClick={downloadFile}
+        >
+          Download
+        </button>
       </div>
       <div className="p-4 pl-8 flex flex-col gap-6 h-full">
         <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
